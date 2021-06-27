55e9246cff14651fc48a08ee59cde5c9d361603b
Masa <katsukatsu.masa.0522@gmail.com>
2021-06-27 11:07:31 +0900

    first commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>Hello, {{ name }}</p>
+</template>
+
+<style scoped>
+h1 {
+  color: teal;
+}
+</style>
+
+<script>
+export default {
+  data () {
+    return {
+      name: 'world'
+    }
+  }
+}
+</script>
